f25c4f603205c2ecf857b9e57fd634a1eabe3176
Goel Nkoko <goelnkoko12@hotmail.com>
2023-08-16 03:36:24 +0100

    feat: Problem O - Big Add and Multiply Refactoring

--- CodeForces/Assiut University Training - Newcomers - Solutions/Grupo 6 - Math/223338O - Big Add and Multiply.cpp
+++ CodeForces/Assiut University Training - Newcomers - Solutions/Grupo 6 - Math/223338O - Big Add and Multiply.cpp
@@ -2,112 +2,68 @@
 
 using namespace std;
 
-string add(string n){
-    string rest;
-    int c, b, i, j;
-    string n2 = "9999";
+string add(string s1, string s2){
+    string r;
 
-    i=n.size()-1;
-    j=n2.size()-1;
-    int r=0, d=0;
+    int s1_t = s1.size();
+    int s2_t = s2.size();
+    int M = max(s1_t, s2_t);
 
-    for(; min(i, j) >= 0; i--, j--){
-        c = n[i]-'0';
-        b = n2[j]-'0';
-        r = (c+b+d)%10;
-        d = (c+b+d)/10;
-        rest.push_back(r+'0');
+    for(int i=min(s1_t, s2_t); i <= M; i++){
+        if(i >= s1_t) s1.insert(s1.begin(), '0');
+        if(i >= s2_t) s2.insert(s2.begin(), '0');
     }
 
-    string a;
-    if(n.size() > n2.size()) a = n;
-    else a = n2;
-
-    i = max(i, j);
-
-    while(i >= 0){
-        c = a[i]-'0';
-        if(d != 0){
-            r = (c+d)%10;
-            d = (c+d)/10;
-            rest += (r+'0');
-        }
-        else {
-            rest += (c+'0');
-        }
-        i--;
+    int last = 0;
+    for(int i=M; i >= 0; i--){
+        int sum = (s1[i]-'0') + (s2[i]-'0') + last;
+        last = sum/10;
+        r.insert(r.begin(), sum%10+'0');
     }
-    reverse(rest.begin(), rest.end());
 
-    if(i < 0 && d != 0) rest = char(d+'0') + rest;
+    while(r[0] == '0')
+        r.erase(r.begin());
     
-    return rest;
+    return r;
 }
 
-string mult(string n){
-    if(n == "0") return "0";
-    string n2 = "9999";
+string multiply(string s1, string s2){ 
+    string r;
 
-    reverse(n.begin(), n.end());
-    reverse(n2.begin(), n2.end());
+    int s1_t = s1.size();
+    int s2_t = s2.size();
+    int M = max(s1_t, s2_t);
 
-    if(n.size() < n2.size()){
-        string s = n;
-        n = n2;
-        n2 = s;
+    for(int i=min(s1_t, s2_t); i <= M; i++){
+        if(i >= s1_t) s1.insert(s1.begin(), '0');
+        if(i >= s2_t) s2.insert(s2.begin(), '0');
+    }
+    for(int i=0; i <= M; i++)
+        r += "00";
+    
+    int last = 0, res = 0, k, t = r.size()-1;
+
+    for(int i=M; i >= 0; i--){
+        for(int j=M, k=t; j >= 0; j--, k--){
+            int mult = (s1[i]-'0')*(s2[j]-'0') + last;
+            last = mult/10;
+            int sum = (mult%10)+(r[k]-'0')+res;
+            res = sum/10;
+            r[k] = sum%10+'0';
+        }
+        t--;
     }
 
-    string rest ="";
-    for(int i=0; i < n.size(); i++)
-        rest += "00";
-     
-    int c, b, r, d = 0, o, div = 0, res, k, t=0;
+    while(r[0] == '0')
+        r.erase(r.begin());
 
-    for(int i=0; i < n2.size(); i++){
-        for(int j = 0, k=i; j < n.size(); j++, k++){
-            c = n[j]-'0';
-            b = n2[i]-'0';
-
-            r = (c*b+d)%10;
-            d = (c*b+d)/10;
-            
-            o = (rest[k]-'0')+r+div;
-
-            div = o/10;
-            res = o%10;
-
-            rest[k] = (res+'0');
-            t = k+1;
-        }
-        if(d != 0){
-            o = (rest[t]-'0')+d+div;
-            div = o/10;
-            res = o%10;
-            rest[t] = (res+'0');
-            d=0;
-            t++;
-        }
-        while(div != 0){
-            o = (rest[t]-'0')+div;
-            div = o/10;
-            res = o%10;
-            rest[t] = (res+'0');
-            t++;
-        }
-    }
-    reverse(rest.begin(), rest.end());
-    while(rest[0] == '0'){
-        rest.erase(rest.begin()+0);
-    }
-    if(div != 0)
-        rest = char(div+'0') + rest;
-    return rest;
+    return r;
 }
 
 int main(){
 
-    string n;
-    cin >> n;
+    string s1, s2;
+    cin >> s1;
 
-    cout << add(n) << "\n" << mult(n) << endl;
+    cout << add(s1, "9999") << "\n" << multiply(s1, "9999")<< endl;
 }
